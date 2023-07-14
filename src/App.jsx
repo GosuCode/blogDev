20f8cddcfecd8d0529a03d1b04fe606928465358
GosuCode <gosucode1945@gmail.com>
2023-07-14 23:04:25 +0545

    createpost

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import Layout from './HOC/navigation/Layout';
 import Index from './component/Index';
 import SinglePost from './component/singlePost/SinglePost';
 import About from './component/pages/About';
-// import CreatePost2 from './component/createPost/CreatePost2';
-import CreatePost1 from '../src/component/createPost/CreatePost1'
 import "react-toastify/dist/ReactToastify.css";
 import Login from './component/Authentication/Login';
 import Register from './component/Authentication/Register';
+import CreatePost from './component/createPost/CreatePost';
 
 function App() {
   return (
@@ -17,8 +16,7 @@ function App() {
           <Routes>
             <Route exact path='/' element={<Index />} />
             <Route exact path='/about' element={<About />} />
-            <Route exact path='/createPost2' element={<CreatePost1 />} />
-            {/* <Route exact path='/createPost2' element={<CreatePost2 />} /> */}
+            <Route exact path='/createPost2' element={<CreatePost />} />
             <Route exact path='/postById/:id' element={<SinglePost />} />
             <Route exact path='/login' element={<Login />} />
             <Route exact path='/register' element={<Register />} />
--- src/component/createPost/CreatePost.jsx
+++ src/component/createPost/CreatePost.jsx
@@ -1,36 +1,110 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import * as yup from 'yup'
+import { useState } from "react";
 import TextEditor from "./TextEditor"
+import axios from "axios";
+
+const validationSchema = yup.object().shape({
+    title: yup.string().required('Title is required.'),
+    description: yup.string().required('Description is required.')
+})
+
 
 const CreatePost = () => {
+    const [showimage, setShowImage] = useState("");
+    const [newImage, setImage] = useState([]);
+
+    const handleImageUpload = (event) => {
+        setShowImage(event.target.files[0]);
+        setImage([...newImage, event.target.files[0]]);
+    };
+
+    const postFormData = async (value) => {
+        const formData = new FormData();
+        formData.append("title", value.title);
+        formData.append("description", value.description);
+        for (let i = 0; i < newImage.length; i++) {
+            formData.append("image", newImage[i]);
+        }
+        console.log(newImage);
+        try {
+            await axios.post("http://localhost:3001/posts/posts", formData);
+            console.trace("who called upon me?")
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const onSubmit = (data) => {
+        console.log('Create Post line:50', data);
+        postFormData(data);
+    }
+
+    const initialValues = {
+        title: '',
+        description: '',
+        image: ''
+    }
+
     return (
         <div className="grid grid-cols-12 gap-4">
-            <div className="col-start-2 col-span-7 h-[500px] bg-white px-16 py-8 rounded-md shadow-md">
-                <div>
-                    <div className="mb-5">
-
-                        <label htmlFor="image" className="border border-slate-500 rounded-sm " >Add a cover image</label>
-
-                    </div>
-                    <div>
-                        <textarea name="description" id="" cols="17" rows="1"
-                            placeholder="New post title here..."
-                            autoComplete="off"
-                            className="text-6xl font-extrabold overflow-hidden focus:outline-none"></textarea>
-                    </div>
-                    <div>
-                        <input className="focus:outline-none" placeholder="Add upto 4 tags"></input>
-                    </div>
-                </div>
-                <div className="w-[650px] bg-white">
-                    <TextEditor />
-                </div>
-            </div>
-            <div className="flex col-start-10 col-span-2">
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}>
+                {({ handleSubmit }) => {
+                    return (
+                        <Form onSubmit={handleSubmit} encType="multipart/form-data"
+                            className="col-start-2 col-span-7 h-[500px] bg-white px-16 py-8 rounded-md shadow-md">
+                            <div>
+                                <div className='mb-5'>
+                                    <img
+                                        src={
+                                            showimage
+                                                ? URL.createObjectURL(showimage)
+                                                : ""
+                                        }
+                                        width={300}
+                                        alt=""
+                                        className="mt-8"
+                                    />
+                                    <Field                       //Input field
+                                        type='file'
+                                        name='image'
+                                        accept=".png,.jpg,.jpeg,.gif"
+                                        multiple
+                                        onChange={(e) => handleImageUpload(e)}
+                                        className=""
+                                    />
+                                </div>
+                                <ErrorMessage
+                                    name='image'
+                                    className='text-red-600 col-span-4'
+                                    component='p' />
+                                <div>
+                                    <textarea name="title" id="" cols="17" rows="1"
+                                        placeholder="New post title here..."
+                                        autoComplete="off"
+                                        className="text-6xl font-extrabold overflow-hidden focus:outline-none"></textarea>
+                                </div>
+                                <div>
+                                    <input className="focus:outline-none" placeholder="Add upto 4 tags"></input>
+                                </div>
+                            </div>
+                            <div className="w-[650px] bg-white">
+                                <TextEditor name='description' />
+                            </div>
+                        </Form>
+                    )
+                }}
+            </Formik>
+            {/* <div className="flex col-start-10 col-span-2">
                 <ul>
                     <h4 className="text-2xl font-bold">How to write a great post title?</h4>
                     <li>Think of your post title as a super short (but compelling!) description — like an overview of the actual post in one short sentence.</li>
                     <li>Use keywords where appropriate to help ensure people can find your post by search.</li>
                 </ul>
-            </div>
+            </div> */}
         </div>
 
     )
